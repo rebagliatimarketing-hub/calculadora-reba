ebef2d145063e2733d3191abdf850d64e7d10c4d
rebagliatimarketing-hub <rebagliati.marketing@gmail.com>
2026-07-09 14:41:00 -0500

    chore: usa sesiones cookie en vercel

--- api/index.php
+++ api/index.php
@@ -1,5 +1,15 @@
 <?php
 
+foreach ([
+    'SESSION_DRIVER' => 'cookie',
+    'CACHE_STORE' => 'array',
+    'QUEUE_CONNECTION' => 'sync',
+] as $key => $value) {
+    putenv("$key=$value");
+    $_ENV[$key] = $value;
+    $_SERVER[$key] = $value;
+}
+
 // Vercel ejecuta PHP en modo serverless; Laravel necesita estas rutas escribibles.
 foreach ([
     '/tmp/views',
